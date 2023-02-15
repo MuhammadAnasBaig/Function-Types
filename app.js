b657db77943a24dbd3d2e183cf36395965feab4d
SajjadAli11 <umarwe04@gmail.com>
2023-02-15 09:33:37 +0500

    functions

---
+++ app.js
@@ -0,0 +1,19 @@
+// //////////    Normal Funtion  ////////////////
+
+function add (x,y){
+     console.log(` ${x+y}`);
+}
+add(2,9);
+
+
+/////////            Expression Function   ////////////////
+
+const meraEs6Expression1 = function (a,b){
+    console.log(`${a-b}`);
+}
+meraEs6Expression1(100,50);
+
+////////////  Arrow Function  //////////////////
+
+const Expression2 = (j,k) => j*k;
+alert(Expression2(5,4))
